7cd055e92c0e7265e73cf73f49af2fa805ae605e
Olaolu Johnson <johnsonolaolu@gmail.com>
2023-08-09 17:28:03 +0100

    fix : updated

--- src/shop/dto/update-notifiaction-token.dto.ts
+++ src/shop/dto/update-notifiaction-token.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateNotificationDto {
-  @IsString() @IsNotEmpty() agentId: string;
+  @IsString() @IsNotEmpty() shopId: string;
   @IsString() @IsNotEmpty() notificationToken: string;
 }
