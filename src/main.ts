be23516a29b8cd6d61590e38a372eb3d2d99f2a3
Olaolu Johnson <johnsonolaolu@gmail.com>
2023-10-08 21:21:48 +0100

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix(`api/${app.get(ConfigService).get('VERSION')}`);
   await app.listen(app.get(ConfigService).get('PORT') || 3000);
