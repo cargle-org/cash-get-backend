b5e3487bf4a3f4db0bc2029094cb7446112c3017
Olaolu Johnson <johnsonolaolu@gmail.com>
2023-10-29 09:47:12 +0100

    updated

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,10 +1,12 @@
-import * as Crypto from 'crypto';
 import { UserEnum } from './constants';
+import * as otpGenerator from 'otp-generator';
 
 export function generateKey(size = 10, role: UserEnum) {
-  const suff = Crypto.randomBytes(size)
-    .toString('base64')
-    .slice(0, size)
-    .toUpperCase();
+  const suff = otpGenerator.generate(size, {
+    digits: true,
+    lowerCaseAlphabets: false,
+    specialChars: false,
+    upperCaseAlphabets: false,
+  });
   return role == UserEnum.AGENT ? `A-${suff}` : `S-${suff}`;
 }
